f620b27c59493476f1539819b67886edfc9f0382
rollerpunk <webmenu@S1>
2018-05-09 20:18:36 +0200

    DB backup 2018-05-09-20:36

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Mdate="$(date +"%Y-%m-%d-%H:%S ")"
+Mdate="$(date +"%Y-%m-%d-%H:%S")"
 mysqldump -uroot -pRoller12# webmenu > /home/webmenu/projects/menu/DBbackups/$Mdate.sql
 cd /home/webmenu/projects/menu/
 git add .
